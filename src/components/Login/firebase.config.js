49188503f8feedc2b7e0bda1ec0e3eee8acf3f95
mehedi345 <mehedihasan5101220@gmail.com>
2020-09-28 22:18:08 -0700

    initial commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBLVGLjR8tSd0Xb49dzU4IRq2wAs3IzyMA",
+    authDomain: "burj-al-arab-cafdf.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-cafdf.firebaseio.com",
+    projectId: "burj-al-arab-cafdf",
+    storageBucket: "burj-al-arab-cafdf.appspot.com",
+    messagingSenderId: "347570681260",
+    appId: "1:347570681260:web:6a75b5f6ae6fda22cab609"
+  };
+  export default firebaseConfig;
